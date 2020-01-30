b2979ca535606be7eb7b9f117157e94f151ee4b8
nomadlife_mac <>
2020-01-30 16:23:36 +0900

    update menulink component

--- src/components/menuLink.js
+++ src/components/menuLink.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { MDBIcon, MDBNavLink } from 'mdbreact';
+// import { MDBIcon, MDBNavLink } from 'mdbreact';
+import { Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const NavLink = ({ to, title }) => {
   return (
-    <MDBNavLink className='list-group-item list-group-item-action' to={to} >
+    <Nav.Link as={Link} className='list-group-item list-group-item-action' to={to} >
       <h5 style={{ margin: '0' }} className='justify-content-between d-flex align-items-center'>
         {title}
-        <MDBIcon icon='angle-right' />
+        {/* <MDBIcon icon='angle-right' /> */}
+        <i className="fas fa-angle-right"></i>
       </h5>
-    </MDBNavLink>
+    </Nav.Link>
   );
 };
 
